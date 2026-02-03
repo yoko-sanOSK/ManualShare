26427e67088ccb4741db5e6c967a05a7f52b4d5a
Eiji Yokota <yokoyokoss11@gmail.com>
2026-02-03 05:39:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7421783370-2e805",
+  "appId": "1:294621972133:web:1645c181cfe9ea0be52f4c",
+  "apiKey": "AIzaSyBX098xI2fFwtylT0sV8ydiWSvi_QghV2U",
+  "authDomain": "studio-7421783370-2e805.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "294621972133"
+};
