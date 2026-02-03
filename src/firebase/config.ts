dcea86608631687a096ba4db0e60068ea1caad52
Eiji Yokota <yokoyokoss11@gmail.com>
2026-02-03 09:14:14 +0000

    このプロジェクトにおける実際の数値を.envにのみ書き込んでほしい

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7421783370-2e805",
-  "appId": "1:294621972133:web:1645c181cfe9ea0be52f4c",
-  "apiKey": "AIzaSyBX098xI2fFwtylT0sV8ydiWSvi_QghV2U",
-  "authDomain": "studio-7421783370-2e805.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "294621972133"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
